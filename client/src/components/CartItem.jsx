607981271737cdffc7dd2b596aef17ce161f5429
Belal-Mohsen <belal4mohsen@gmail.com>
2023-12-13 23:24:01 -0500

    Added the CartPage

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,34 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { FaRegTrashCan } from "react-icons/fa6";
+import { IoMdArrowDropup, IoMdArrowDropdown } from "react-icons/io";
+import PropTypes from 'prop-types';
 
-const CartItem = ({cartItem}) => {
+const CartItem = ({ item, onIncrease, onDecrease, onDelete }) => {
+  const { name, image, quantity, price } = item;
+
   return (
-    <div className="absolute w-[1268px] h-[353px] top-[266px] left-[86px]">
-            <div className="inline-flex flex-col items-start gap-[10px] absolute top-0 left-0">
-              <div className="relative w-[1268px] h-[100px] bg-white rounded-[15px] shadow-[0px_1px_4px_#00000040]" />
-              <div className="absolute w-[14px] top-[39px] left-[681px] [font-family:'Raleway',Helvetica] font-semibold text-[#393939] text-[22px] tracking-[0] leading-[22px] whitespace-nowrap">
-                1
-              </div>
-              <div className="absolute w-[219px] top-[35px] left-[124px] [font-family:'Poppins',Helvetica] font-medium text-[#1e1e1e] text-[18px] tracking-[0] leading-[normal]">
-                {cartItem.name}
-              </div>
-              <img
-                className="absolute w-[92px] h-[82px] top-[9px] left-[12px]"
-                alt="Rectangle"
-                src="/img/rectangle-19-2.svg"
-              />
-              <img className="absolute w-[23px] h-[20px] top-[40px] left-[702px]" alt="Group" src="/img/group-4.png" />
-              <div className="absolute w-[69px] top-[43px] left-[1029px] [font-family:'Poppins',Helvetica] font-medium text-[#393939] text-[14px] text-right tracking-[0] leading-[normal]">
-                ${cartItem.price}
-              </div>
-              <img
-                className="absolute w-[29px] h-[25px] top-[37px] left-[1169px]"
-                alt="Trash can"
-                src="/img/trash-can.png"
-              />
-            </div>
-            
-          </div>
-  )
-}
+    <div className="bg-white rounded-[20px] shadow-md p-4 mb-4 flex flex-col md:flex-row items-center justify-between mx-32">
 
-export default CartItem
+      <div className="flex items-center">
+        <img src={image} alt={name} className="w-12 h-12 rounded-lg object-cover mr-8" />
+        <div>
+          <p className="text-md font-semibold">{name}</p>
+        </div>
+      </div>
+
+      <div className="flex items-center">
+        <span className="mx-2">{quantity}</span>
+        <div className='flex flex-col items-center'>
+          <button onClick={onIncrease} disabled={quantity === 1}><IoMdArrowDropup className='text-2xl' /></button>
+          <button onClick={onDecrease}><IoMdArrowDropdown className='text-2xl' /></button>
+        </div>
+
+
+      </div>
+
+      <div className="flex items-center">
+        <p className="text-md font-medium md:mx-16">${price}</p>
+        <button onClick={onDelete} className="md:mx-16">
+          <FaRegTrashCan className='text-2xl' />
+        </button>
+      </div>
+
+    </div>
+  );
+};
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default CartItem;
--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,19 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopBanner from "../components/TopBanner";
 import NavBar from "../components/NavBar";
 import Shadow from "../components/Shadow";
 import Footer from "../components/Footer";
-import Newsletter from '../components/Newsletter';
+import Newsletter from "../components/Newsletter";
+import CartItem from "../components/CartItem";
 
 const CartPage = () => {
+
+  const [cartItems, setCartItems] = useState([
+    // TODO: implement the real implementation
+    // Dummy Data for testing
+    { id: 1, name: 'Item 1', image: "./images/image1.png", quantity: 2, price: 10 },
+    { ide: 2, name: 'Item 2', image: "./images/image1.png", quantity: 1, price: 20 },
+
+  ]);
+
+  const handleIncrease = (itemId) => {
+    const updatedCart = cartItems.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+
+    setCartItems(updatedCart);
+  };
+
+  const handleDecrease = (itemId) => {
+    const updatedCart = cartItems.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, quantity: Math.max(1, item.quantity - 1) };
+      }
+      return item;
+    });
+
+    setCartItems(updatedCart);
+  };
+
+
+  const handleDelete = (itemId) => {
+    const updatedCart = cartItems.filter((item) => item.id !== itemId);
+    setCartItems(updatedCart);
+  };
 
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <TopBanner />
       <NavBar />
       <Shadow />
-      <div className="bg-white flex flex-row justify-center w-full">
-        test
+      <div className="bg-white flex flex-col justify-center items-center w-full flex-grow">
+        <h1 className='font-semibold'>Shopping cart</h1>
+        <div className='w-full'>
+          {cartItems.map((item, index) => (
+            <CartItem key={index} item={item}
+              onIncrease={() => handleIncrease(item.id)}
+              onDecrease={() => handleDecrease(item.id)}
+              onDelete={() => handleDelete(item.id)}
+            />
+          ))}
+        </div>
+
+        <div className='flex w-full px-32 justify-between mt-4'>
+
+          <div className='flex items-start'>
+            <input
+              className='border p-2 rounded-md border-black mr-4'
+              placeholder='Coupon Code'
+            />
+            <button
+              className="px-4 py-2 bg-[#c0876a] text-white rounded-md"
+            >
+              Apply Coupon
+            </button>
+          </div>
+
+          <div className='flex flex-col border border-gray-400/25 rounded-md'>
+            <p className='mx-3 my-8'>Cart Total</p>
+            <div className='flex justify-between mx-4'> <p className='mr-72'>Subtotal: </p> <p className='mr-6'>${ }</p></div>
+            <div className='flex justify-between mx-4'> <p className='mr-72'>Shipping: </p> <p className='mr-6'>Free</p></div>
+            <div className='flex justify-between mx-4'> <p className='mr-72'>Total: </p> <p className='mr-6'>${ }</p></div>
+
+            <div className='flex justify-center justify-items-center mb-6'>
+              <button
+                className="px-4 py-2 bg-[#c0876a] text-white rounded-md w-1/2"
+              >
+                CheckOut
+              </button>
+            </div>
+
+          </div>
+
+
+        </div>
       </div>
       <Newsletter />
       <Footer />
