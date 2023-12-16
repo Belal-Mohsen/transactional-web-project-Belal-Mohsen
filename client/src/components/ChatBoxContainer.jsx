18d2d01acfee630738259cc8c11fc393b04609d9
Chelsea Yao <szyao416@gmail.com>
2023-12-15 23:20:41 -0500

    added chatbot container

---
+++ client/src/components/ChatBoxContainer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
+import { useState } from "react";
+
+const ChatBoxContainer = () => {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am a chatbot!",
+      sender: "Chatbot",
+    },
+  ]);
+
+  const handelSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing",
+    };
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+    // [TODO! Process messages!]
+  };
+
+  return (
+    <div style={{ position: "relative", height: "500px", width: "600px" }}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            {messages.map((message, i) => {
+              return <Message key={i} model={message} />;
+            })}
+          </MessageList>
+          <MessageInput placeholder="Type message here" onSend={handelSend} />
+        </ChatContainer>
+      </MainContainer>
+    </div>
+  );
+};
+
+export default ChatBoxContainer;
