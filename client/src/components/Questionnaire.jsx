6e660391d0c9214ae61cea06cded088de63b53b3
Diana Majolli Andre <dianamajolli@hotmail.com>
2023-12-17 16:57:30 -0500

    Updated questionnaire page- added black border on select image

--- client/src/components/Questionnaire.jsx
+++ client/src/components/Questionnaire.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Questionnaire = () => {
+  const [selectedImages, setSelectedImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  });
+
+  const handleImageClick = (questionNumber, imageNumber) => {
+    console.log(`Clicked Image ${imageNumber} for Question ${questionNumber}`);
+
+    setSelectedImages((prevSelectedImages) => ({
+      ...prevSelectedImages,
+      [questionNumber]: imageNumber,
+    }));
+  };
+
+  const getImageStyle = (questionNumber, imageNumber) => {
+    return {
+      border:
+        selectedImages[questionNumber] === imageNumber
+          ? "1px solid black"
+          : "none",
+    };
+  };
+
   return (
-    <div className="flex flex-wrap w-full max-w-[1000px] mx-auto mt-4">
+    <div className="flex flex-wrap w-full max-w-[1150px] mx-auto mt-4">
       {/* First block - Top left */}
       <div className="w-full md:w-1/2 text-center mb-4 md:mb-0">
-        <div style={{ marginRight: "0.7vw", marginLeft: "0.5vw", marginTop: "0.5vw", marginBottom: "1.5vw" }}>
+        <div
+          style={{
+            marginRight: "0.5vw",
+            marginLeft: "0.7vw",
+            marginTop: "0.5vw",
+            marginBottom: "5.5vw",
+          }}
+        >
           <span className="text-stone-800 text-xl font-bold font-['Inter'] leading-tight block">
             Question 1/4
           </span>
@@ -13,52 +45,37 @@ const Questionnaire = () => {
             Which image best captures your decorating style?
           </span>
           <div className="flex justify-between mt-4">
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 1
-                console.log("Clicked Image 1");
-              }}
-            >
-              <img
-                className="w-[154px] h-[203px] md:w-[168px] md:h-[198px] lg:w-[146px] lg:h-[150px]"
-                src="./images/Subscribe_img1.png"
-                alt="Image 1"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 2
-                console.log("Clicked Image 2");
-              }}
-            >
-              <img
-                className="w-[168px] h-[198px] md:w-[168px] md:h-[198px] lg:w-[168px] lg:h-[150px]"
-                src="./images/Subscribe_img2.png"
-                alt="Image 2"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 3
-                console.log("Clicked Image 3");
-              }}
-            >
-              <img
-                className="w-[146px] h-[200px] md:w-[168px] md:h-[198px] lg:w-[146px] lg:h-[150px]"
-                src="./images/Subscribe_img3.png"
-                alt="Image 3"
-              />
-            </button>
+            {[1, 2, 3].map((imageNumber) => (
+              <button
+                key={imageNumber}
+                className="focus:outline-none"
+                onClick={() => handleImageClick(1, imageNumber)}
+              >
+                <img
+                  className="w-[154px] h-[203px] md:w-[168px] md:h-[198px] lg:w-[155px] lg:h-[150px]"
+                  src={`./images/Subscribe_img${imageNumber}.png`}
+                  alt={`Image ${imageNumber}`}
+                  style={getImageStyle(1, imageNumber)}
+                />
+              </button>
+            ))}
           </div>
         </div>
       </div>
 
+      {/* Add more space between blocks */}
+      <div className="w-full md:w-1/12"></div>
+
       {/* Second block - Top right */}
-      <div className="w-full md:w-1/2 text-center mb-4 md:mb-0">
-        <div style={{ marginRight: "0.5vw", marginLeft: "4.5vw", marginTop: "0.5vw", marginBottom: "4.5vw" }}>
+      <div className="w-full md:w-5/12 text-center mb-4 md:mb-0">
+        <div
+        // style={{
+        //    marginRight: "0.5vw",
+        //    marginLeft: "4.5vw",
+        //    marginTop: "0.5vw",
+        //    marginBottom: "4.5vw",
+        //  }}
+        >
           <span className="text-stone-800 text-xl font-bold font-['Inter'] leading-tight block">
             Question 2/4
           </span>
@@ -66,52 +83,33 @@ const Questionnaire = () => {
             Which area of the home are you decorating?
           </span>
           <div className="flex justify-between mt-4">
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 4
-                console.log("Clicked Image 4");
-              }}
-            >
-              <img
-                className="w-[154px] h-[203px] md:w-[168px] md:h-[198px] lg:w-[146px] lg:h-[150px]"
-                src="./images/Subscribe_img4.png"
-                alt="Image 4"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 5
-                console.log("Clicked Image 5");
-              }}
-            >
-              <img
-                className="w-[168px] h-[198px] md:w-[168px] md:h-[198px] lg:w-[168px] lg:h-[150px]"
-                src="./images/Subscribe_img5.png"
-                alt="Image 5"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 6
-                console.log("Clicked Image 6");
-              }}
-            >
-              <img
-                className="w-[146px] h-[200px] md:w-[168px] md:h-[198px] lg:w-[146px] lg:h-[150px]"
-                src="./images/Subscribe_img6.png"
-                alt="Image 6"
-              />
-            </button>
+            {[4, 5, 6].map((imageNumber) => (
+              <button
+                key={imageNumber}
+                className="focus:outline-none"
+                onClick={() => handleImageClick(2, imageNumber)}
+              >
+                <img
+                  className="w-[154px] h-[203px] md:w-[168px] md:h-[198px] lg:w-[146px] lg:h-[150px]"
+                  src={`./images/Subscribe_img${imageNumber}.png`}
+                  alt={`Image ${imageNumber}`}
+                  style={getImageStyle(2, imageNumber)}
+                />
+              </button>
+            ))}
           </div>
         </div>
       </div>
-
       {/* Third block - Below first block */}
       <div className="w-full md:w-1/2 text-center mb-4 md:mb-0">
-        <div style={{ marginRight: "0.7vw", marginLeft: "0.5vw", marginTop: "-0.25vw", marginBottom: "2.5vw" }}>
+        <div
+          style={{
+            marginRight: "0.5vw",
+            marginLeft: "0.7vw",
+            marginTop: "-0.25vw",
+            marginBottom: "2.5vw",
+          }}
+        >
           <span className="text-stone-800 text-xl font-bold font-['Inter'] leading-tight block">
             Question 3/4
           </span>
@@ -119,65 +117,37 @@ const Questionnaire = () => {
             What is your favorite holiday?
           </span>
           <div className="flex justify-between mt-4">
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 7
-                console.log("Clicked Image 7");
-              }}
-            >
-              <img
-                className="w-[100px] h-[150px] md:w-[100px] md:h-[50px] lg:w-[110px] lg:h-[130px]"
-                src="./images/Subscribe_img7.png"
-                alt="Image 7"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 8
-                console.log("Clicked Image 8");
-              }}
-            >
-              <img
-                className="w-[100px] h-[150px] md:w-[100px] md:h-[50px] lg:w-[110px] lg:h-[130px]"
-                src="./images/Subscribe_img8.png"
-                alt="Image 8"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 9
-                console.log("Clicked Image 9");
-              }}
-            >
-              <img
-                className="w-[100px] h-[150px] md:w-[100px] md:h-[50px] lg:w-[110px] lg:h-[120px]"
-                src="./images/Subscribe_img9.png"
-                alt="Image 9"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 10
-                console.log("Clicked Image 10");
-              }}
-            >
-              <img
-                className="w-[100px] h-[150px] md:w-[100px] md:h-[50px] lg:w-[110px] lg:h-[120px]"
-                src="./images/Subscribe_img10.png"
-                alt="Image 10"
-              />
-            </button>
+            {[17, 7, 8, 9, 10, 11, 12].map((imageNumber) => (
+              <button
+                key={imageNumber}
+                className="focus:outline-none"
+                onClick={() => handleImageClick(3, imageNumber)}
+              >
+                <img
+                  className="w-[154px] h-[203px] md:w-[168px] md:h-[198px] lg:w-[150px] lg:h-[130px]"
+                  src={`./images/Subscribe_img${imageNumber}.png`}
+                  alt={`Image ${imageNumber}`}
+                  style={getImageStyle(3, imageNumber)}
+                />
+              </button>
+            ))}
           </div>
         </div>
       </div>
 
+      {/* Add more space between blocks */}
+      <div className="w-full md:w-1/12"></div>
+
       {/* Fourth block - Below second block */}
-      <div className="w-full md:w-1/2 text-center mb-4 md:mb-0">
-        <div style={{ marginRight: "0.5vw", marginLeft: "6.5vw", marginTop: "-0.25vw", marginBottom: "4.5vw" }}>
+      <div className="w-full md:w-5/12 text-center mb-4 md:mb-0">
+        <div
+          style={{
+            marginRight: "0.5vw",
+            marginLeft: "0.7vw",
+            marginTop: "-0.25vw",
+            marginBottom: "2.5vw",
+          }}
+        >
           <span className="text-stone-800 text-xl font-bold font-['Inter'] leading-tight block">
             Question 4/4
           </span>
@@ -185,59 +155,20 @@ const Questionnaire = () => {
             What is your favorite color?
           </span>
           <div className="flex justify-between mt-4">
-           
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 13
-                console.log("Clicked Image 13");
-              }}
-            >
-              <img
-                className="w-[80px] h-[80px] md:w-[100px] md:h-[50px] lg:w-[60px] lg:h-[60px]"
-                src="./images/Subscribe_img13.png"
-                alt="Image 13"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 14
-                console.log("Clicked Image 14");
-              }}
-            >
-              <img
-                className="w-[80px] h-[80px] md:w-[100px] md:h-[50px] lg:w-[60px] lg:h-[60px]"
-                src="./images/Subscribe_img14.png"
-                alt="Image 14"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 15
-                console.log("Clicked Image 15");
-              }}
-            >
-              <img
-                className="w-[80px] h-[80px] md:w-[100px] md:h-[50px] lg:w-[60px] lg:h-[60px]"
-                src="./images/Subscribe_img15.png"
-                alt="Image 15"
-              />
-            </button>
-            <button
-              className="focus:outline-none"
-              onClick={() => {
-                // Handle click for Image 16
-                console.log("Clicked Image 16");
-              }}
-            >
-              <img
-                className="w-[80px] h-[80px] md:w-[100px] md:h-[50px] lg:w-[60px] lg:h-[60px]"
-                src="./images/Subscribe_img16.png"
-                alt="Image 16"
-              />
-            </button>
+            {[13, 14, 15, 16, 22, 23, 24].map((imageNumber) => (
+              <button
+                key={imageNumber}
+                className="focus:outline-none"
+                onClick={() => handleImageClick(4, imageNumber)}
+              >
+                <img
+                  className="w-[80px] h-[80px] md:w-[100px] md:h-[50px] lg:w-[60px] lg:h-[60px]"
+                  src={`./images/Subscribe_img${imageNumber}.png`}
+                  alt={`Image ${imageNumber}`}
+                  style={getImageStyle(4, imageNumber)}
+                />
+              </button>
+            ))}
           </div>
         </div>
       </div>
