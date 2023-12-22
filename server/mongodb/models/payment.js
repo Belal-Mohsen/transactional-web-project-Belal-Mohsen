950738c4b4b4e37fa1fba5dc0585ceb258a4d50a
Belal-Mohsen <belal4mohsen@gmail.com>
2023-12-22 10:03:33 -0500

    Added payment model to db

---
+++ server/mongodb/models/payment.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from 'mongoose';
+
+const Payment = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    order: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Order'
+    },
+    paymentMEthod: {
+        type: String,
+        required: true
+    },
+    paymentResult: {
+        id: { type: String },
+        status: { type: String },
+        updateTime: { type: String },
+        emailAddress: { type: String },
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    paidAt: {
+        type: Date
+    }
+},
+    {
+        timestamps: ture
+    });
+
+const PaymentSchema = mongoose.model('Payment', Payment);
+
+export default PaymentSchema;
+
+
--- server/mongodb/models/user.js
+++ server/mongodb/models/user.js
@@ -1,23 +1,72 @@
 import mongoose, { Schema } from 'mongoose';
 
 const subscriptionSchema = new Schema({
-    style: { type: String },
-    area: { type: String },
-    holidays: { type: [{ type: String }] },
-    color: { type: String },
-    images: { type: [{ type: String }] },
-    date: { type: Date, default: Date.now }
-});
+    style: {
+        type: String,
+        required: true
+    },
+    area: {
+        type: String,
+        required: true
+    },
+    holidays: {
+        type: [{ type: String }],
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: [{ type: String }],
+        required: true
+    },
+    date: {
+        type: Date, default: Date.now,
+        required: true
+    }
+},
+    {
+        timestamps: true
+    });
 
 const User = new Schema({
-    fName: { type: String, required: true },
-    lName: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true, trim: true },
-    address: { type: String },
-    newsLetter: { type: Boolean, default: false },
+    fName: {
+        type: String,
+        required: true
+    },
+    lName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    address: {
+        type: String
+    },
+    newsLetter: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+
+    },
     subscription: subscriptionSchema
-});
+},
+    {
+        timestamps: ture
+    });
 
 const UserSchema = mongoose.model('User', User);
 
