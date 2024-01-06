87d6acd1b4afcd2e2f4d901d17d27048e533f167
Belal-Mohsen <belal4mohsen@gmail.com>
2024-01-05 23:02:08 -0500

    Updated the url for Stripe: Trying to solve Stripe problem

--- server/routes/stripeRoutes.js
+++ server/routes/stripeRoutes.js
@@ -30,8 +30,8 @@ router.route('/').post(async (req, res) => {
             payment_method_types: ['card'],
             line_items: lineItems,
             mode: 'payment',
-            success_url: 'http://localhost:3000/success',
-            cancel_url: 'http://localhost:3000/cancel',
+            success_url: 'https://main.d2ctub1uon7ubc.amplifyapp.com/success',
+            cancel_url: 'https://main.d2ctub1uon7ubc.amplifyapp.com/cancel',
         });
 
         res.json({ id: session.id });
